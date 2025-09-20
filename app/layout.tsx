0862b45426bf0ebf08e6e555408599d065aca3a1
kdjadeja21 <krushnasinh.jadeja14545@marwadieducation.edu.in>
2025-09-20 14:41:14 +0530

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,27 @@ export const metadata: Metadata = {
   title: "Fallbacks | A Gallery of React Error Boundary Templates",
   description:
     "Explore 20+ ready-made error boundary component templates for React applications",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" }, // fallback for older browsers
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+      },
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
